e366be91fe8804b56a5a735f3a8a362140fb302e
davidjoliver86 <davidjoliver.2021@gmail.com>
2024-04-21 09:14:48 -0700

    chrome

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt update
-apt install -y git python3-venv
+apt install -y python3-venv python3-psutil
 /usr/bin/python3 -m venv /usr/lib/virtualenvs/ansible --system-site-packages
 
 /usr/lib/virtualenvs/ansible/bin/pip install --upgrade pip
